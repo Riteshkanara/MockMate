6dad5a525c3b0f4f25bdf2fd7ab6ce93bee34943
Ritesh Kanara <riteshkanara7777@gmail.com>
2026-08-16 12:01:02 +0530

    feat(auth): add useAuth hook

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,5 @@
+// hooks/useAuth.js
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+const useAuth = () => useContext(AuthContext);
+export default useAuth;
